522115b529320f0036319fe0b694a925e55d5719
Sasha <ashihm2006@gmail.com>
2025-06-27 13:12:09 +0300

    Дебаг и тестирование Qwen3-0.6B

--- task_generate.py
+++ task_generate.py
@@ -1,24 +1,21 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import prompts_for_tasks
 
-Qwen_name = "Qwen/Qwen2.5-1.5B-Instruct-AWQ"
+Qwen2_name = "Qwen/Qwen2.5-1.5B-Instruct"
 Vikhr_name = "Vikhrmodels/QVikhr-2.5-1.5B-Instruct-r"
+Qwen3_name = "Qwen/Qwen3-0.6B"
 
 models = {
-    "qwen": AutoModelForCausalLM.from_pretrained(Qwen_name),
-    "vikhr": AutoModelForCausalLM.from_pretrained(Vikhr_name)
+    "qwen2": AutoModelForCausalLM.from_pretrained(Qwen2_name),
+    "vikhr": AutoModelForCausalLM.from_pretrained(Vikhr_name),
+    "qwen3": AutoModelForCausalLM.from_pretrained(Qwen3_name)
 }
 
 tokenizers = {
-    "qwen": AutoTokenizer.from_pretrained(Qwen_name),
-    "vikhr": AutoTokenizer.from_pretrained(Vikhr_name)
+    "qwen2": AutoTokenizer.from_pretrained(Qwen2_name),
+    "vikhr": AutoTokenizer.from_pretrained(Vikhr_name),
+    "qwen3": AutoTokenizer.from_pretrained(Qwen3_name)
 }
-
-Qwen_tokenizer = AutoTokenizer.from_pretrained(Qwen_name)
-Vikhr_tokenizer = AutoTokenizer.from_pretrained(Vikhr_name)
-
-Qwen_model = AutoModelForCausalLM.from_pretrained(Qwen_name)
-Vikhr_model = AutoModelForCausalLM.from_pretrained(Vikhr_name)
 
 YOU_AI = """
 Вы — ИИ помощник, созданный для
@@ -35,7 +32,8 @@ def generate_danetqa(text, question, name):
     answer = tokenizer.decode(
         output[0][len(input_ids[0]):],
         skip_special_tokens=True).strip()
-    return 1 if (answer == "да" or answer == "Да") else 0
+    #return answer
+    return 1 if (answer[0:2] == "да" or answer[0:2] == "Да") else 0
 
 
 def generate_terra(premise, hypothesis, name):
@@ -47,10 +45,11 @@ def generate_terra(premise, hypothesis, name):
     answer = tokenizer.decode(
         output[0][len(input_ids[0]):],
         skip_special_tokens=True).strip()
-    return 1 if answer == "entailment" else 0
+    #return answer
+    return 0 if answer[0:10] == "entailment" else 1
 
 
-SYSTEM_PROMPT_PARUS = "Отвечай на вопрос только 0 или 1"
+SYSTEM_PROMPT_PARUS = "Отвечай на вопрос только 1 или 2"
 
 
 def generate_parus(premise: str, choice1: str, choice2: str, question: str, name: str):
@@ -60,42 +59,34 @@ def generate_parus(premise: str, choice1: str, choice2: str, question: str, name
         choice2,
         question
     )
-    messages = {
-        {'role': 'system', 'content': SYSTEM_PROMPT_PARUS},
-        {"role": "system", "content": YOU_AI},
-        {"role": "user", "content": input_text},
-    }
-
     model = models[name]
     tokenizer = tokenizers[name]
 
-    input_ids = tokenizer.apply_chat_template(
-        messages, truncation=True,
-        add_generation_prompt=True,
-        return_tensors="pt"
-    )
-    output = model.generate(
-        input_ids,
-        max_length=512,
-        temperature=0.4,
-    )
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+    output = model.generate(input_ids)
     answer = tokenizer.decode(
         output[0][len(input_ids[0]):],
-        skip_special_tokens=True
-    )
-    return 1 if answer == 1 else 2
+        skip_special_tokens=True).strip()
+    #return answer
+    index1 = answer.find("1")
+    index2 = answer.find("2")
+    if (index1 != -1 and index2 != -1):
+        return 0 if (index1 < index2) else 1
+    return 1
+    
+    #return 1 if answer == 1 else 2
 
 
-SYSTEM_PROMPT_RWSD = "Отвечай на вопрос только true или false"
+SYSTEM_PROMPT_RWSD = "Отвечай на задание только true или false"
 
 
 def generate_rwsd(text: str, span1: str, span2: str, name: str):
     input_text = prompts_for_tasks.rwsd_prompt(text, span1, span2)
-    messages = {
+    messages = [
         {'role': 'system', 'content': SYSTEM_PROMPT_PARUS},
         {"role": "system", "content": YOU_AI},
         {"role": "user", "content": input_text},
-    }
+    ]
     model = models[name]
     tokenizer = tokenizers[name]
     input_ids = tokenizer.apply_chat_template(
@@ -105,13 +96,14 @@ def generate_rwsd(text: str, span1: str, span2: str, name: str):
     output = model.generate(
         input_ids,
         max_length=512,
-        temperature=0.4,
     )
     answer = tokenizer.decode(
         output[0][len(input_ids[0]):],
         skip_special_tokens=True
     )
-    return 1 if answer == "true" else 0
+    if (len(answer) == 0):
+        return 0
+    return 1 if answer[0:4] == "true" else 0
 
 
 def generate_rucos(passage, query, entities, name):
@@ -126,9 +118,6 @@ def generate_rucos(passage, query, entities, name):
     return answer
 
 
-SYSTEM_PROMPT_RCB = "Отвечай на вопрос только true или false"
-
-
 def generate_rcb(premise, hypothesis, negation, verb, name):
     input_text = prompts_for_tasks.rcb_prompt(
         premise,
@@ -136,35 +125,20 @@ def generate_rcb(premise, hypothesis, negation, verb, name):
         negation,
         verb
     )
-    messages = {
-        {'role': 'system', 'content': SYSTEM_PROMPT_RCB},
-        {"role": "system", "content": YOU_AI},
-        {"role": "user", "content": input_text},
-    }
+    
     model = models[name]
     tokenizer = tokenizers[name]
-    input_ids = tokenizer.apply_chat_template(
-        messages, truncation=True,
-        add_generation_prompt=True,
-        return_tensors="pt")
-    output = model.generate(
-        input_ids,
-        max_length=512,
-        temperature=0.4,
-    )
-    answer = tokenizer.decode(
-        output[0][len(input_ids[0]):],
-        skip_special_tokens=True
-    )
-    if (answer == "0"):
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+    output = model.generate(input_ids, max_new_tokens=512)
+    answer = tokenizer.decode(output[0][len(input_ids[0]):],skip_special_tokens=True)
+    if (len(answer) == 0):
         return 0
-    elif (answer == "1"):
+    if (answer[0] == "0"):
+        return 0
+    elif (answer[0] == "1"):
         return 1
     else:
         return 2
-
-
-SYSTEM_PROMPT_MUSERC = "Отвечай на вопрос только 0 или 1"
 
 
 def generate_muserc(text, question, answer, name):
@@ -173,30 +147,19 @@ def generate_muserc(text, question, answer, name):
         question,
         answer,
     )
-    messages = {
-        {'role': 'system', 'content': SYSTEM_PROMPT_MUSERC},
-        {"role": "system", "content": YOU_AI},
-        {"role": "user", "content": input_text},
-    }
     model = models[name]
     tokenizer = tokenizers[name]
-    input_ids = tokenizer.apply_chat_template(
-        messages, truncation=True,
-        add_generation_prompt=True,
-        return_tensors="pt")
-    output = model.generate(
-        input_ids,
-        max_length=512,
-        temperature=0.4,
-    )
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+    output = model.generate(input_ids)
     answer = tokenizer.decode(
         output[0][len(input_ids[0]):],
         skip_special_tokens=True
     )
-    return 1 if answer == "1" else 0
+    #return answer
+    if (len(answer) == 0):
+        return 0
+    return 1 if answer[0] == "1" else 0
 
-
-SYSTEM_PROMPT_RUSSE_WIC = "Отвечай на только 0 или 1"
 
 
 def generate_russe_wic(word: str, sentence1: str, sentence2: str, name:str):
@@ -205,27 +168,16 @@ def generate_russe_wic(word: str, sentence1: str, sentence2: str, name:str):
         sentence1,
         sentence2,
     )
-    messages = {
-        {'role': 'system', 'content': SYSTEM_PROMPT_RUSSE_WIC},
-        {"role": "system", "content": YOU_AI},
-        {"role": "user", "content": input_text},
-    }
     model = models[name]
     tokenizer = tokenizers[name]
-    input_ids = tokenizer.apply_chat_template(
-        messages, truncation=True,
-        add_generation_prompt=True,
-        return_tensors="pt")
-    output = model.generate(
-        input_ids,
-        max_length=512,
-        temperature=0.4,
-    )
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+    output = model.generate(input_ids)
     answer = tokenizer.decode(
         output[0][len(input_ids[0]):],
         skip_special_tokens=True
     )
-    return 1 if answer == "1" else 0
+    #return answer
+    return 1 if answer[0] == "1" else 0
 
 
 SYSTEM_PROMPT_LIDIRUS = "Отвечай на только 0 или 1"
@@ -247,11 +199,11 @@ def generate_lidirus(
         logic,
         predicate_argument_structure,
     )
-    messages = {
+    messages = [
         {'role': 'system', 'content': SYSTEM_PROMPT_LIDIRUS},
         {"role": "system", "content": YOU_AI},
         {"role": "user", "content": input_text},
-    }
+    ]
     model = models[name]
     tokenizer = tokenizers[name]
     input_ids = tokenizer.apply_chat_template(
